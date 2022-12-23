4236e56893a3af133d461b95ce01ad32b62a8637
elasri21 <islame.edu@gmail.com>
2022-12-23 22:18:33 +0100

    files were added

---
+++ main.js
@@ -0,0 +1,117 @@
+const openNav = document.querySelector(".open");
+const closeNav = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+openNav.addEventListener("click", function() {
+    this.style.display = "none";
+    closeNav.style.display = "block";
+    nav.style.display = "block";
+});
+
+closeNav.addEventListener("click", function() {
+    this.style.display = "none";
+    openNav.style.display = "block";
+    nav.style.display = "none";
+});
+
+// 
+
+const downs = Array.from(document.querySelectorAll(".down"));
+downs.forEach(down => {
+    down.addEventListener("click",function() {
+        this.children[1].style.display = "flex";
+    });
+});
+
+
+//
+const bgImages = [
+    "../images/landing-01",
+    "../images/landing-02",
+    "../images/landing-03"
+];
+let start = 0;
+const landing = document.querySelector(".landing");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+
+/*
+arrowLeft.addEventListener("click", function() {
+    console.log("clicked")
+    if(start <= 0) {
+        landing.style.backgroundImage.url = bgImages[bgImages.length - 1];
+    } else {
+        start--;
+        landing.style.backgroundImage.url = bgImages[start];
+        console.log(start)
+    }
+});
+arrowRight.addEventListener("click", function() {
+    console.log("clicked")
+    if(start >= bgImages.length - 1) {
+        landing.style.backgroundImage.url = bgImages[0];
+    } else {
+        start++;
+        landing.style.backgroundImage.url = bgImages[start];
+        console.log(start)
+    }
+});
+*/
+
+// panels
+
+const titles = Array.from(document.querySelectorAll(".titles span"));
+const boxes = Array.from(document.querySelectorAll(".boxes .box"));
+
+titles.forEach(title => {
+    title.addEventListener("click", function() {
+        for(let i = 0; i < titles.length; i++) {
+            titles[i].classList.remove("active");
+        }
+        boxes.forEach(box => {
+            if(this.id == box.dataset.id) {
+                box.classList.remove("hide");
+            } else {
+                box.classList.add("hide");
+            }
+        });
+        this.classList.add("active");
+    });
+});
+
+//
+
+const testBoxes = Array.from(document.querySelectorAll(".testimonials .box"));
+const bullets = Array.from(document.querySelectorAll(".bullets span"));
+
+bullets.forEach(bullet => {
+    bullet.addEventListener("click", function () {
+        for(let i = 0; i < bullets.length; i++) {
+            bullets[i].classList.remove("active");
+        }
+        testBoxes.forEach(box => {
+            if(this.id == box.dataset.id) {
+                box.classList.remove("hide");
+            } else {
+                box.classList.add("hide");
+            }
+        });
+        this.classList.add("active");
+    });
+});
+
+// 
+const header = document.querySelector("header");
+const up = document.querySelector(".up");
+window.addEventListener("scroll", function () {
+    if(window.scrollY == 0) {
+        header.style.backgroundColor = "transparent";
+        up.style.display = "none";
+    }else if(window.scrollY > 150) {
+        header.style.backgroundColor = "#000000cc";
+    }
+    if(this.window.scrollY > 500) {
+        up.style.display = "flex";
+    }
+});
+
